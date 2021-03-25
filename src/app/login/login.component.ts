ae791deed43b6161cea8b16a00a2989428a1e250
Vishal Rindhe <rindhevishal32@gmail.com>
2021-03-25 14:40:12 +0530

    authguard implemented

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule,Router } from '@angular/router';
 
 import { LoginClass } from '../login-class';
 import { NgForm } from '@angular/forms';
+import { DataService } from '../data.service';
 
 
 
@@ -10,20 +11,38 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers:[DataService]
 })
 export class LoginComponent implements OnInit {
+
   
   userModel = new LoginClass("","")
   // topicHasError= true
   onSubmit(){
     this.router.navigate(["/dashboard"])
   }
-  constructor(public router:Router) { }
+  constructor(public router:Router, private service: DataService) { }
 
   ngOnInit(): void {
   }
 
+  msg!: string;
+
+  check(uname: string, p : string)
+  {
+    var output = this.service.checkusernameandpassword(uname, p);
+    if(output == true)
+    {
+      this.router.navigate(['/dashboard/1']);
+    }
+    else{
+    this.msg ='Invalid username or password';
+    alert(this.msg)
+    console.log(this.msg);
+    
+    }
+  }
 //   btnClick(){
 //     this.router.navigateByUrl('/dashboard');
 //   }
